95aab669c90994afa7132d30be011759ee903503
Paul Bombarde <pbombarde@logitech.com>
2024-12-07 17:05:13 +0100

    Day 7-2

--- day7/lib/day7.dart
+++ day7/lib/day7.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'dart:io';
 import 'dart:convert';
 
+typedef Operator =int Function(int,int);
 class Equation extends Equatable {
   final int res;
   final List<int> vals;
@@ -18,34 +19,56 @@ class Equation extends Equatable {
   }
 
   bool isValid() {
-    return _isValidRange(vals[0], 1);
+    return _isValidRange(vals[0], 1, [add, mult]);
   }
 
-  bool _isValidRange(int acc, int start) {
+  bool isValid2() {
+    return _isValidRange(vals[0], 1, [add, mult, combine]);
+  }
+
+  bool _isValidRange(int acc, int start, List<Operator> ops) {
     if (start == vals.length) return acc == res;
-    if (_isValidRange(acc + vals[start], start + 1)) return true;
-    if (_isValidRange(acc * vals[start], start + 1)) return true;
+    for(var op in ops){
+      if (_isValidRange(op(acc, vals[start]), start + 1, ops)) {
+        return true;
+      }
+    }
     return false;
+  }
+
+  static int combine(int high, int low){
+    int f = 1;
+    while(f <= low) f *= 10;
+    return high * f + low;
+  }
+
+  static int add(int a, int b){
+    return a + b;
+  }
+
+  static int mult(int a, int b){
+    return a * b;
   }
 }
 
 class ValidSum {
-  int sum = 0;
+  int sum1 = 0;
+  int sum2 = 0;
 
-  static Future<int> fromFile(String filePath) async {
+  static Future<(int, int)> fromFile(String filePath) async {
     var v = ValidSum();
     await for (var line in readLines(filePath)) {
       v.addLine(line);
     }
-    return v.sum;
+    return (v.sum1, v.sum2);
   }
 
-  static int addLines(List<String> lines) {
+  static (int, int) addLines(List<String> lines) {
     var v = ValidSum();
     for (var line in lines) {
       v.addLine(line);
     }
-    return v.sum;
+    return (v.sum1, v.sum2);
   }
 
   void addLine(String line) {
@@ -53,7 +76,8 @@ class ValidSum {
   }
 
   void addEquation(Equation equation) {
-    if (equation.isValid()) sum += equation.res;
+    if (equation.isValid()) sum1 += equation.res;
+    if (equation.isValid2()) sum2 += equation.res;
   }
 }
 
--- day7/test/day7_test.dart
+++ day7/test/day7_test.dart
@@ -24,7 +24,38 @@ void main() {
       expect(Equation(21037, [9, 7, 18, 13]).isValid(), false);
       expect(Equation(292, [11, 6, 16, 20]).isValid(), true);
     });
+
+    test('is valid 2', () {
+      expect(Equation(11, [11]).isValid2(), true);
+      expect(Equation(11, [12]).isValid2(), false);
+      expect(Equation(190, [10, 19]).isValid2(), true);
+      expect(Equation(29, [10, 19]).isValid2(), true);
+      expect(Equation(29, [11, 19]).isValid2(), false);
+
+      expect(Equation(3267, [81, 40, 27]).isValid2(), true);
+      expect(Equation(83, [17, 5]).isValid2(), false);
+      expect(Equation(156, [15, 6]).isValid2(), true);
+      expect(Equation(7290, [6, 8, 6, 15]).isValid2(), true);
+      expect(Equation(161011, [16, 10, 13]).isValid2(), false);
+      expect(Equation(192, [17, 8, 14]).isValid2(), true);
+      expect(Equation(21037, [9, 7, 18, 13]).isValid2(), false);
+      expect(Equation(292, [11, 6, 16, 20]).isValid2(), true);
+    });
+
+    test('combine ints', (){
+      expect(Equation.combine(1, 2), 12);
+      expect(Equation.combine(11, 22), 1122);
+      expect(Equation.combine(1, 2345), 12345);
+      expect(Equation.combine(12, 345), 12345);
+      expect(Equation.combine(123, 45), 12345);
+      expect(Equation.combine(1234, 5), 12345);
+      expect(Equation.combine(12340, 5), 123405);
+      expect(Equation.combine(1234, 50), 123450);
+      expect(Equation.combine(1234, 10), 123410);
+    });
   });
+
+
 
   group('ValidSum', () {
     test('add valid lines', () {
@@ -40,7 +71,7 @@ void main() {
         '292: 11 6 16 20'
       ];
       
-      expect(ValidSum.addLines(lines), 3749);
+      expect(ValidSum.addLines(lines), (3749, 11387));
     });
   });
 }
