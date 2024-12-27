88163767c2b6038876ab9b4d855c6bc368a2f72e
Paul Bombarde <pbombarde@logitech.com>
2024-12-27 22:11:14 +0100

    Day22-2

--- day22/lib/day22.dart
+++ day22/lib/day22.dart
@@ -1,36 +1,81 @@
 import 'dart:io';
+import 'dart:math';
 
-int calculate(String filePath){
+(int, int) calculate(String filePath) {
   var inputs = File(filePath).readAsLinesSync();
   return calculateFromStrings(inputs);
 }
 
-int calculateFromStrings(Iterable<String> inputs){
-  var generator = SecretGenerator();
+(int, int) calculateFromStrings(Iterable<String> inputs) {
   var intInputs = inputs.map(int.parse);
-  var secrets2000 = intInputs.map((s) => generator.nextIter(s, 2000));
-  return secrets2000.fold(0, (a,b) => a+b);
+  var secrets2000 = intInputs.map((s) => nextIter(s, 2000));
+  var best = bestSequence(intInputs, 2000);
+  return (secrets2000.fold(0, (a, b) => a + b), best.$2);
 }
 
-class SecretGenerator{ 
-  int nextIter(int secret, int req){
-    int s = secret;
-    for(int i=0; i<req; ++i) s = next(s);
-    return s;
+((int, int, int, int), int) bestSequence(Iterable<int> secrets, int req) {
+  var ds = DeltasMap();
+  for (var s in secrets) {
+    var bs = bananas(s, req);
+    for (var e in deltas(bs).entries) {
+      ds[e.key] = (ds[e.key] ?? 0) + e.value;
+    }
   }
 
-  int next(int secret){
-    int n = prune(mix(secret, secret * 64));
-    n = prune(mix(n, n ~/ 32));
-    n = prune(mix(n, n * 2048));
-    return n;
+  var m = ds.values.reduce(max);
+  for (var d in ds.entries) {
+    if (d.value == m) return (d.key, d.value);
   }
+  return ((0, 0, 0, 0), 0);
 }
 
-int mix(int secret, int val){
+int nextIter(int secret, int req) {
+  int s = secret;
+  for (int i = 0; i < req; ++i) s = next(s);
+  return s;
+}
+
+int next(int secret) {
+  int n = prune(mix(secret, secret * 64));
+  n = prune(mix(n, n ~/ 32));
+  n = prune(mix(n, n * 2048));
+  return n;
+}
+
+List<int> bananas(int secret, int req) {
+  int prev = secret;
+  var res = <int>[];
+  for (int i = 0; i < req; ++i) {
+    res.add(prev % 10);
+    prev = next(prev);
+  }
+  return res;
+}
+
+typedef DeltasMap = Map<(int, int, int, int), int>;
+DeltasMap deltas(List<int> bananas) {
+  var d = DeltasMap();
+  for (int i = 4; i < bananas.length; ++i) {
+    var key = (
+      delta(bananas, i - 3),
+      delta(bananas, i - 2),
+      delta(bananas, i - 1),
+      delta(bananas, i)
+    );
+    if (d.containsKey(key)) continue;
+    d[key] = bananas[i];
+  }
+  return d;
+}
+
+int delta(List<int> bananas, int i) {
+  return bananas[i] - bananas[i - 1];
+}
+
+int mix(int secret, int val) {
   return secret ^ val;
 }
 
-int prune(int secret){
+int prune(int secret) {
   return secret % 16777216;
 }
--- day22/test/day22_test.dart
+++ day22/test/day22_test.dart
@@ -7,34 +7,83 @@ void main() {
     expect(calculateFromStrings(inputs), 37327623);
   });
 
-  test('mix', (){
-    expect(mix(42,15), 37);
+  test('mix', () {
+    expect(mix(42, 15), 37);
   });
 
-  test('prune', (){
+  test('prune', () {
     expect(prune(100000000), 16113920);
   });
 
-  test('next secret', (){
-    var g = SecretGenerator();
-
-    expect(g.next(123), 15887950);
-    expect(g.next(15887950), 16495136);
-    expect(g.next(16495136), 527345);
-    expect(g.next(527345), 704524);
-    expect(g.next(704524), 1553684);
-    expect(g.next(1553684), 12683156);
-    expect(g.next(12683156), 11100544);
-    expect(g.next(11100544), 12249484);
-    expect(g.next(12249484), 7753432);
-    expect(g.next(7753432), 5908254);
+  test('next secret', () {
+    expect(next(123), 15887950);
+    expect(next(15887950), 16495136);
+    expect(next(16495136), 527345);
+    expect(next(527345), 704524);
+    expect(next(704524), 1553684);
+    expect(next(1553684), 12683156);
+    expect(next(12683156), 11100544);
+    expect(next(11100544), 12249484);
+    expect(next(12249484), 7753432);
+    expect(next(7753432), 5908254);
   });
 
-  test('next 2000', (){
-    var g = SecretGenerator();
-    expect(g.nextIter(1, 2000), 8685429);
-    expect(g.nextIter(10, 2000), 4700978);
-    expect(g.nextIter(100, 2000), 15273692);
-    expect(g.nextIter(2024, 2000), 8667524);
+  test('next 2000', () {
+    expect(nextIter(1, 2000), 8685429);
+    expect(nextIter(10, 2000), 4700978);
+    expect(nextIter(100, 2000), 15273692);
+    expect(nextIter(2024, 2000), 8667524);
+  });
+
+  test('bananas', () {
+//      123: 3
+// 15887950: 0 (-3)
+// 16495136: 6 (6)
+//   527345: 5 (-1)
+//   704524: 4 (-1)
+//  1553684: 4 (0)
+// 12683156: 6 (2)
+// 11100544: 4 (-2)
+// 12249484: 4 (0)
+//  7753432: 2 (-2)
+
+    var bs = bananas(123, 10);
+    expect(bs, [3, 0, 6, 5, 4, 4, 6, 4, 4, 2]);
+    expect(deltas(bs), {
+      (-3, 6, -1, -1): 4,
+      (6, -1, -1, 0): 4,
+      (-1, -1, 0, 2): 6,
+      (-1, 0, 2, -2): 4,
+      (0, 2, -2, 0): 4,
+      (2, -2, 0, -2): 2,
+    });
+  });
+
+  test('one by one 1', (){
+    var bs = bananas(1, 2000);
+    var ds = deltas(bs);
+    expect(ds[(-2,1,-1,3)], 7);
+  });
+
+  test('one by one 2', (){
+    var bs = bananas(2, 2000);
+    var ds = deltas(bs);
+    expect(ds[(-2,1,-1,3)], 7);
+  });
+
+  test('one by one 3', (){
+    var bs = bananas(3, 2000);
+    var ds = deltas(bs);
+    expect(ds.containsKey((-2,1,-1,3)), false);
+  });
+
+  test('one by one 2024', (){
+    var bs = bananas(2024, 2000);
+    var ds = deltas(bs);
+    expect(ds[(-2,1,-1,3)], 9);
+  });
+
+  test('best seq', (){
+    expect(bestSequence([1,2,3,2024], 2000), ((-2,1,-1,3), 23));
   });
 }
